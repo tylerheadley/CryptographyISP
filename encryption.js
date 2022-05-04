24d2bf693cd6a2f1f3325b4a3e974749d5c49c4f
tylerheadley <84789498+tylerheadley@users.noreply.github.com>
2022-05-04 10:59:44 -0400

    Add files via upload

--- encryption.js
+++ encryption.js
@@ -1,3 +1,16 @@
 function caesarCipherEncrypt(plaintext) {
-  return plaintext + "!";
+  let ciphertext = "";
+  const key = Math.floor(Math.random() * 25 + 1); // generate random key 1-25
+  for (let i = 0; i < plaintext.length; i++) {
+    const asciiVal = plaintext.charCodeAt(i);
+    let newAscii = asciiVal;
+    if (asciiVal >= 97 && asciiVal <= 122 || asciiVal >= 65 && asciiVal <= 90) {
+      newAscii += key;
+      if (newAscii > 122) {
+        newAscii = newAscii - 26;
+      }
+    }
+    ciphertext += String.fromCharCode(asciiVal);
+  }
+  return ciphertext;
 }
