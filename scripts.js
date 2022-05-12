82405e52686253e22088c69be49f1010cdebee65
tylerheadley <84789498+tylerheadley@users.noreply.github.com>
2022-05-12 17:59:45 -0400

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-function readForm() {
-    const plaintext = document.getElementById("text").elements[0].value;
-    document.getElementById("displayInput").innerHTML = `Your input is ${plaintext}`;
-    
-    key = Math.floor(Math.random() * 25 + 1); // generate random number 1-25
-    document.getElementById("displayKey").innerHTML = `Random key: ${key}`;
-    const ciphertext = caesarCipherEncrypt(plaintext, key);
-    document.getElementById("displayOutput").innerHTML = `Your output is ${ciphertext}`;
-  }
