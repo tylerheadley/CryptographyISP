9b7d0f4714ecc6537fccf9a7af8c80aa8a129a18
tylerheadley <84789498+tylerheadley@users.noreply.github.com>
2022-06-02 10:14:17 -0400

    Add files via upload

--- caesarcipher.js
+++ caesarcipher.js
@@ -1,5 +1,5 @@
 function caesarCipherEncrypt() {
-  const plaintext = document.getElementById("inputtext");
+  const plaintext = document.getElementById("inputtext").value;
   document.getElementById("displayInput").innerHTML = `Your input is <strong>${plaintext}</strong>`;
   let key = parseInt(document.getElementById("inputkey").value);
   if (!key) { // if key was not inputted it will have a falsy value
@@ -27,5 +27,29 @@ function caesarCipherEncrypt() {
     ciphertext += String.fromCharCode(newAscii);
   }
   document.getElementById("displayOutput").innerHTML = `Your output is <strong>${ciphertext}</strong>`;
+  return ciphertext;
+}
+
+function caesarCipher(plaintext, key) {
+  let ciphertext = "";
+
+  for (let i = 0; i < plaintext.length; i++) {
+    // get ASCII value
+    const asciiVal = plaintext.charCodeAt(i);
+    let newAscii = asciiVal;
+
+    // if character is a letter, apply shift
+    if (asciiVal >= 97 && asciiVal <= 122 || asciiVal >= 65 && asciiVal <= 90) {
+      newAscii += key;
+
+      // wrap around if value is too high
+      if (newAscii > 122 || asciiVal < 97 && newAscii > 90) {
+        newAscii -= 26;
+      }
+    }
+
+    // add new character to ciphertext
+    ciphertext += String.fromCharCode(newAscii);
+  }
   return ciphertext;
 }
